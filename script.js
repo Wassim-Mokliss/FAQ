db34ffea2d7dc5645b18a0d5e158ae914cf7c386
Wassim <wasshimu.dev@gmail.com>
2024-12-04 06:27:30 +0100

    Finition boucle forEach + amélioration css

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 console.log('connecté !');
 // Selectionne et stock elements dans un tableau
-const questions = document.querySelectorAll(".questions");
+const questions = document.querySelectorAll(".question");
 console.log(questions);
 
-// Boucle forEach pour ajout evenement sur chacun des tableaux
+// Boucle forEach pour ajout evenement sur chacun des elements du tabeau
 questions.forEach((item, index)=>{
     console.log(item,index,'item');
+    item.addEventListener('click', function(){
+        console.log(`Question ${index} cliqué`);
+        // Je cible l'element suivant de chaque item donc question qui font reference aux questions
+        const next = item.nextElementSibling;
+        console.log(next);
+        // Affichage/disparition de la réponse
+        next.classList.toggle('visible');
+        // Je change aussi l'icone 
+        icone = item.lastElementChild;
+        icone.classList.toggle('fa-chevron-up');
+    })
 });
